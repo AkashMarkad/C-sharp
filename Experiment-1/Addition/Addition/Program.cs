d0def4aaeaa31d593bef7e70cee00164bba16309
AkashMarkad <79374621+AkashMarkad@users.noreply.github.com>
2022-03-21 15:54:50 +0530

    Add files via upload

---
+++ Experiment-1/Addition/Addition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int i, j, n, sum = 0;
+
+
+            Console.WriteLine("enter how many number you want to add : ");
+
+            n = int.Parse(Console.ReadLine());
+
+            int[] a = new int[n];
+
+            Console.WriteLine("Enter " + n + " numbers : ");
+
+            for (i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (j = 0; j < a.Length; j++)
+            {
+                sum = sum + a[j];
+            }
+
+            Console.WriteLine("Sum : " + sum);
+
+            Console.ReadKey();
+        }
+    }
+}
